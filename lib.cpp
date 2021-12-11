d867966b434817787dde34c2d72879e088f6ee5b
Albertopostani <91824639+Albertopostani@users.noreply.github.com>
2021-12-11 21:39:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+#include "libj.h"
+float funzione(const float matrice[],int a){
+    float max=matrice[0];
+    for(int i=1;i<a;i++){
+        if(matrice[i]>max){
+            max=matrice[i];
+        }
+    }
+    return max;
+}
